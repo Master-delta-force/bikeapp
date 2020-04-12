90a1d84e298ab6c900195373dc8666caaa36600a
Omar Flores Grimontt <omar.flores@3dves.com>
2020-04-12 09:29:49 -0500

    [ADD] created image component

--- src/components/atoms/Image.svelte
+++ src/components/atoms/Image.svelte
@@ -1,30 +1,46 @@
 <script>
     export let src
     export let alt
+    export let caption
 </script>
 
 <style>
     div {
-        border-radius: var(--common-border-radius);
-        display: block;
         height: 0;
-        overflow: hidden;
         padding: 56.25% 0 0 0;
         position: relative;
         width: 100%;
     }
 
     img {
+        border-radius: var(--common-border-radius);
         box-shadow: var(--common-container-shadow);
         height: 100%;
         object-fit: cover;
         position: absolute;
         top: 0;
         width: 100%;
-        animation: fadeInImage 1s ease;
+        animation: fadeInImage 1s;
+    }
+
+    figure {
+        margin: calc(var(--common-spacing-unit) * 3) 0;
+    }
+
+    figcaption {
+        word-break: break-word;
+        padding: 0;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+        padding-top: 8px;
     }
 </style>
 
-<div>
-    <img src={src} alt={alt}/>
-</div>
+<figure>
+    <div>
+        <img src={src} alt={alt}/>
+    </div>
+    <figcaption title={caption}>{caption}</figcaption>
+</figure>
