adde2d4a50005cf35a5d5094d42e257fe26b3005
AryRosvall <ary.rosvall@gmail.com>
2020-04-16 23:25:35 -0500

    [ADD] Card Atom and Atoms folder

---
+++ src/components/atoms/Card.svelte
@@ -0,0 +1,20 @@
+<style>
+	.card {
+		display: flex;
+		flex-flow: column nowrap;
+		border-radius: 4px;
+		box-shadow: 2px 2px 8px rgba(0,0,0,0.1);
+		padding: 1em;
+		margin: 8px;
+		background-color: #fff;
+		color: #1a272f;
+		overflow: hidden;
+	}
+
+</style>
+
+<div class="card">
+	<slot>
+		<em>no content was provided</em>
+	</slot>
+</div>
