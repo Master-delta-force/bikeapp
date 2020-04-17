4bed42c003222a2e4d537eeec2b892f68b3689a0
Omar Flores Grimontt <omar.flores@3dves.com>
2020-04-17 09:00:59 -0500

    [FIX] Removed comments to avoid server to connect to Firebase

--- src/server.js
+++ src/server.js
@@ -2,13 +2,13 @@ import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
-// import bikes from "../api/bikes";
+import bikes from "../api/bikes";
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
 polka()
-	// .use('/api/v1/bikes', bikes)
+	.use('/api/v1/bikes', bikes)
 	.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
