7ee1ed083e3e3313d870fad3b0b3a470e31dae23
AryRosvall <ary.rosvall@gmail.com>
2020-04-16 23:45:21 -0500

    [ADD] Avatar Atom

---
+++ src/components/atoms/Avatar.svelte
@@ -0,0 +1,17 @@
+<script>
+
+export let src;
+let className;
+export {className as class}
+
+</script>
+
+<style>
+.avatar {
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+}
+</style>
+
+<img {src} alt="Avatar" class={className}>
