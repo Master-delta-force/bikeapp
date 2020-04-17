fbb8452a2bb622db6cc88f6a0a9bce8f86b6c188
AryRosvall <ary.rosvall@gmail.com>
2020-04-16 23:38:03 -0500

    [ADD] Button Atom

---
+++ src/components/atoms/Button.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let text;
+  export let onClick;
+  export let disabled = false;
+  let className = "";
+	export { className as class };
+
+</script>
+
+<style>
+</style>
+
+<button on:click={onClick} disabled={disabled} class={className}>
+{text}
+</button>
