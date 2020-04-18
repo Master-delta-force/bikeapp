599a3a9deb768ae9f3433da2abd171978069318f
Omar Flores Grimontt <omar.flores@3dves.com>
2020-04-17 20:44:05 -0500

    [FIX] Removed component from home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,5 @@
-<script>
-import Alert from '../components/atoms/Alert.svelte'
-</script>
-
 <svelte:head>
   <title>BikeApp</title>
 </svelte:head>
 
 <h1>Hola Mundo</h1>
-
-<Alert open infinite message="holaaa"/>
