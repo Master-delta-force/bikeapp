79bae55368b1faba2502978a1bf28df5d87721e8
Fabian Toro <helber.toro@gmail.com>
2020-04-18 19:08:09 -0500

    Notification counter atom

---
+++ src/components/atoms/Notification.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let number = 0;
+  export let className = '';
+</script>
+
+<style>
+  .notification {
+    display: flex;
+    width: 35px;
+    height: 35px;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    background: var(--common-color-secondary-light);
+    color: white;
+    font-size: var(--common-font-size);
+  }
+</style>
+
+<span class={`notification ${className}`}>{number}</span> 
