6e67ea0609ba21084ee21446d2dd34f779924796
Jose Manuel Salazar <soltisam@gmail.com>
2020-04-20 15:25:13 -0500

    add users api

--- src/server.js
+++ src/server.js
@@ -3,12 +3,18 @@ import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 import bikes from "../api/bikes";
+import users from "../api/users";
+import brands from '../api/brands';
+import types_bikes from '../api/types_bikes';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
 polka()
 	.use('/api/v1/bikes', bikes)
+	.use('/api/v1/users', users)
+	.use('/api/v1/brands', brands)
+	.use('/api/v1/types_bikes', types_bikes)
 	.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
